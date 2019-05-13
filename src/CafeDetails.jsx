9dc6ea7a06fcf286a20a9b3c336a3dfd327e80be
Stephanie Saretsky <stephanie@Stephanies-MacBook-Pro-2.local>
2019-05-13 16:43:59 -0400

    homepage

--- src/CafeDetails.jsx
+++ src/CafeDetails.jsx
@@ -5,7 +5,15 @@ import { Link } from "react-router-dom";
 
 class CafeDetails extends Component {
   render = () => {
-    return <div />;
+    // return (
+    //   <div>
+    //     whatever the layout is
+    //     <h1>Is there room?</h1>
+    //     if (seatcount = 0){" "}
+    //     {<p>There is no room. Try coming by in 15 minutes</p>}
+    //     <p>There is {this.x} seats left. Come on by!</p>
+    //   </div>
+    // );
   };
 }
 
