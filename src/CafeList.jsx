d2d8500e421f0cfdb7caea9dca2b5647c39ad1aa
Stephanie Saretsky <stephanie@Stephanies-MacBook-Pro-2.local>
2019-05-13 12:32:42 -0400

    cafe list and search

--- src/CafeList.jsx
+++ src/CafeList.jsx
@@ -1,2 +1,84 @@
-import React, { Component } from 'react'
-import main.css from "/main.css"
+import React, { Component } from "react";
+import "./css/search.css";
+import "./css/main.css";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+class UnconnectedCafeList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cafes: [],
+      searchInput: ""
+    };
+  }
+
+  componentDidMount = () => {
+    console.log("shop list rendering");
+    fetch(path + "cafes", {
+      method: "GET"
+    })
+      .then(x => {
+        return x.text();
+      })
+      .then(responseBody => {
+        let body = JSON.parse(responseBody);
+        if (body.success) {
+          this.setState({ cafes: body.cafeList });
+        }
+      });
+  };
+
+  handleChange = event => {
+    console.log(event.target.value);
+    let newInput = event.target.value;
+    this.setState({ searchInput: newInput });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    let search = this.state.searchInput;
+    fetch(path + "search?search=" + search)
+      .then(response => response.text())
+      .then(response => {
+        let parsedResponse = JSON.parse(response);
+        console.log("Response", parsedResponse);
+        if (parsedResponse.success) {
+          console.log("array of search", parsedResponse);
+          this.setState({ cafes: parsedResponse });
+        }
+      })
+      .catch(err => console.log(err));
+    this.setState({ searchInput: "" });
+  };
+
+  render = () => {
+    return (
+      <div>
+        <h1>Is There Room At Your Fave Cafe?</h1>
+        <form className="search" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            className="searchTerm"
+            value={this.state.searchInput}
+            onChange={this.handleChange}
+            placeholder="Search cafes"
+          />
+        </form>
+        <ul className="cafe-list-container">
+          {this.state.cafes.map(cafe => {
+            return (
+              <div className="cafe-card">
+                <Link to={"cafe/" + cafe._id}>{cafe.name}</Link>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  };
+}
+
+let CafeList = connect()(UnconnectedCafeList);
+
+export default CafeList;
