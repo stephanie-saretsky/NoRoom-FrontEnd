8b1c642f8b92c669f1b424a2ddb2c828dccb62d2
Stephanie Saretsky <stephanie@Stephanies-MacBook-Pro-2.local>
2019-05-15 15:34:34 -0400

    app details

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,17 @@ import Homepage from "./Homepage.jsx";
 import Footer from "./Footer.jsx";
 import Owner from "./Owner/Owner.jsx";
 import NavBar from "./NavBar.jsx";
+import CafeDetails from "./CafeDetails.jsx";
 let path = "http://localhost:4000/";
 
 class UnconnectedApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cafe: [],
+      reviews: []
+    };
+  }
   componentDidMount = () => {
     fetch(path + "login-check", {
       credentials: "include"
@@ -39,7 +47,36 @@ class UnconnectedApp extends Component {
     return <AllCafes />;
   };
 
-  renderCafeDetails = () => {};
+  renderCafeDetails = routerData => {
+    console.log(routerData, "ROUTERDATA");
+    let cafeId = routerData.match.params.cid;
+    let data = new FormData();
+    data.append("cafeId", cafeId);
+    console.log("CAFE=>", cafeId);
+    fetch(path + "cafe-details", {
+      method: "POST",
+      body: data
+    })
+      .then(x => {
+        return x.text();
+      })
+      .then(responseBody => {
+        let body = JSON.parse(responseBody);
+        if (body.success) {
+          console.log("result=>", body.cafe);
+          console.log("Reviews=>", body.reviews);
+          if (this.state.cafe._id !== body.cafe._id) {
+            this.setState({ cafe: body.cafe, reviews: body.reviews });
+          }
+        }
+      });
+    // console.log(this.state.cafe, "CAFE STATE");
+    // this.props.dispatch({
+    //   type: "cafe-results",
+    //   cafes: this.state.cafe
+    // });
+    return <CafeDetails cafe={this.state.cafe} reviews={this.state.reviews} />;
+  };
 
   render = () => {
     return (
@@ -48,7 +85,11 @@ class UnconnectedApp extends Component {
         <div className="global">
           <Route exact={true} path="/" render={this.renderHomepage} />
           <Route exact={true} path="/cafes" render={this.renderCafes} />
-          <Route exact={true} path="/cafes/:cid" render={this.cafeDetails} />
+          <Route
+            exact={true}
+            path="/cafe/:cid"
+            render={this.renderCafeDetails}
+          />
         </div>
         <Footer />
       </BrowserRouter>
