db3c9e7951aac9802dd80ba83aae67947423a642
Stephanie Saretsky <stephanie@Stephanies-MacBook-Pro-2.local>
2019-05-17 17:01:31 -0400

    fixed details loading bug

--- src/CafeDetails.jsx
+++ src/CafeDetails.jsx
@@ -116,90 +116,92 @@ class UnconnectedCafeDetails extends Component {
         }
       }
     });
-    return (
-      <div className="section">
-        <div className="main-details">
-          <div className="section-wrapper">
-            <div className="section-content details-text">
-              <h1 className="room-title">
-                {"Is there room at " + cafe.name + "?"}
-              </h1>
-              {seatDiv}
+    if (this.state.cafe._id)
+      return (
+        <div className="section">
+          <div className="main-details">
+            <div className="section-wrapper">
+              <div className="section-content details-text">
+                <h1 className="room-title">
+                  {"Is there room at " + cafe.name + "?"}
+                </h1>
+                {seatDiv}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="main-details">
-          <div className="section-wrapper">
-            <div className="section-content details">
-              {chairs.map(chair => {
-                if (chair.taken) {
+          <div className="main-details">
+            <div className="section-wrapper">
+              <div className="section-content details">
+                {chairs.map(chair => {
+                  if (chair.taken) {
+                    return (
+                      <img
+                        height={(50 / 730) * 100 + "%"}
+                        src="/chair-taken.png"
+                        style={{
+                          position: "absolute",
+                          left: (chair.x / 1000) * 100 + "%",
+                          top: (chair.y / 730) * 100 + "%",
+                          zIndex: 10
+                        }}
+                      />
+                    );
+                  }
+                  if (!chair.taken) {
+                    return (
+                      <img
+                        height={(50 / 730) * 100 + "%"}
+                        src="/chair.png"
+                        style={{
+                          position: "absolute",
+                          left: (chair.x / 1000) * 100 + "%",
+                          top: (chair.y / 730) * 100 + "%",
+                          zIndex: 10
+                        }}
+                      />
+                    );
+                  }
+                })}
+                {tables.map(table => {
                   return (
                     <img
-                      height={(50 / 730) * 100 + "%"}
-                      src="/chair-taken.png"
+                      height={(60 / 730) * 100 + "%"}
+                      src="/table.png"
                       style={{
                         position: "absolute",
-                        left: (chair.x / 1000) * 100 + "%",
-                        top: (chair.y / 730) * 100 + "%",
+                        left: (table.x / 1000) * 100 + "%",
+                        top: (table.y / 730) * 100 + "%",
                         zIndex: 10
                       }}
                     />
                   );
-                }
-                if (!chair.taken) {
-                  return (
-                    <img
-                      height={(50 / 730) * 100 + "%"}
-                      src="/chair.png"
-                      style={{
-                        position: "absolute",
-                        left: (chair.x / 1000) * 100 + "%",
-                        top: (chair.y / 730) * 100 + "%",
-                        zIndex: 10
-                      }}
-                    />
-                  );
-                }
-              })}
-              {tables.map(table => {
-                return (
-                  <img
-                    height={(60 / 730) * 100 + "%"}
-                    src="/table.png"
-                    style={{
-                      position: "absolute",
-                      left: (table.x / 1000) * 100 + "%",
-                      top: (table.y / 730) * 100 + "%",
-                      zIndex: 10
-                    }}
-                  />
-                );
-              })}
+                })}
+              </div>
+            </div>
+          </div>
+          <div className="main-details">
+            <div className="section-wrapper">
+              <div className="section-content details-map">
+                <Map />
+              </div>
+            </div>
+          </div>
+          <div className="main-details">
+            <div className="section-wrapper">
+              <div className="section-content details-contact">
+                <h2 className="contact-title">{"Contact " + cafe.name}</h2>
+                <p className="contact-p">{cafe.address}</p>
+                <p className="contact-p">{cafe.city + " " + cafe.code}</p>
+                <p className="contact-p">{cafe.number}</p>
+                <p>
+                  <a href={cafe.url}>Website</a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="main-details">
-          <div className="section-wrapper">
-            <div className="section-content details-map">
-              <Map />
-            </div>
-          </div>
-        </div>
-        <div className="main-details">
-          <div className="section-wrapper">
-            <div className="section-content details-contact">
-              <h2 className="contact-title">{"Contact " + cafe.name}</h2>
-              <p className="contact-p">{cafe.address}</p>
-              <p className="contact-p">{cafe.city + " " + cafe.code}</p>
-              <p className="contact-p">{cafe.number}</p>
-              <p>
-                <a href={cafe.url}>Website</a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+      );
+    else return <div />;
   };
 }
 
